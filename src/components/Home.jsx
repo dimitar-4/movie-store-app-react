0959adbb446d1e04060bdeea7d915a1486dc914e
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-08 16:02:56 +0100

    Add basic layout for featured movie in Home comp.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="row p-5">
+            <div className="col-md-6 p-4">
+                <h1>The Godfather</h1>
+                <div>
+                    <p>1994</p>
+                    <p>|</p>
+                    <p>Drama</p>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Sapiente veniam modi, unde temporibus ullam asperiores, 
+                    sit voluptas officiis dolore placeat, deserunt laborum 
+                    dicta tempore saepe consequuntur magni assumenda sint officia.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Sapiente veniam modi, unde temporibus ullam asperiores, 
+                    sit voluptas officiis dolore placeat, deserunt laborum 
+                    dicta tempore saepe consequuntur magni assumenda sint officia.
+                </p>
+            </div>
+            <div className="col-md-6 p-4 bg-secondary rounded"></div>
         </div>
     )
 }
