310ce5eeb46db2b8a6975a6766b9c6ddea1410a9
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-08 17:25:15 +0100

    Create Footer comp & add content & style to footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    const date = new Date();
+
+    return (
+        <footer className="py-4 bg-dark text-white mt-5">
+            <p className="text-center m-0">Copyright &copy; - {date.getFullYear()} The Movie Store</p>
+            <p style={{fontSize: 12}} className="text-center m-0 fw-bold fst-italic">All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
