af23a47dc28d71f59c8fb63f47635031c44c4ca2
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-08 17:26:05 +0100

    Update navbar brand content.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Movie Store</Link>
+                <Link className="navbar-brand" to="/">The Movie Store</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
