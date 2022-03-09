d37c4d162920249c3bf01207e5b4aad552f58b0b
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-09 15:29:52 +0100

    Add BS positioning classes to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     const date = new Date();
 
     return (
-        <footer className="py-4 bg-dark text-white mt-5">
+        <footer className="py-4 bg-dark text-white mt-auto">
             <p className="text-center m-0">Copyright &copy; - {date.getFullYear()} The Movie Store</p>
             <p style={{fontSize: 12}} className="text-center m-0 fw-bold fst-italic">All Rights Reserved</p>
         </footer>
