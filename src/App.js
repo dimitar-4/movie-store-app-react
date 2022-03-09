3a6dfa42df90ab88abe427b1113dca78712a78f0
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-09 15:33:31 +0100

    Import & add new routes. Add BS style classes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import MovieDetails from "./components/MovieDetails";
 import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
+import Orders from "./components/Orders";
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
