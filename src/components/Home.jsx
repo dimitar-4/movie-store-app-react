cca0a21add5f3f4e7a4ccbfc1c91fea45f417764
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-09 17:40:16 +0100

    Add imports, fetch API data & fix featured movie.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,47 @@
-import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
+    const [loading, setLoading] = useState(true);
+    const [featured, setFeatured] = useState();
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/api/movies")
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                setMovies(json.data.slice(0, 4))
+                setFeatured(json.data[4])
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            })
+    }, [])
+
     return (
         <div>
             <div className="row p-5">
                 <div className="col-md-6 p-4">
-                    <h1 className="mb-4">The Godfather</h1>
+                    <h1 className="mb-4">{featured.title}</h1>
                     <div className="d-flex fw-bold">
-                        <p className="me-1">1994</p>
+                        <p className="me-1">{featured.year}</p>
                         <p className="me-1">|</p>
-                        <p>Drama</p>
+                        <p>{featured.genres.map((g, i) => 
+                            i === featured.genres.length - 1 ? g : g + ", "
+                        )}</p>
                     </div>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Sapiente veniam modi, unde temporibus ullam asperiores, 
-                        sit voluptas officiis dolore placeat, deserunt laborum 
-                        dicta tempore saepe consequuntur magni assumenda sint officia.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Sapiente veniam modi, unde temporibus ullam asperiores, 
-                        sit voluptas officiis dolore placeat, deserunt laborum 
-                        dicta tempore saepe consequuntur magni assumenda sint officia.
+                        {featured.description}
                     </p>
-                    <p className="fw-bold">129.99 SEK</p>
+                    <p className="fw-bold">{featured.price} {featured.currency}</p>
                     <button className="btn btn-primary">Purchase</button>
                 </div>
                 <div className="col-md-6 p-4">
-                    <img src="https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY98_CR1,0,67,98_AL_.jpg" 
-                        alt="" className="d-block h-100 mx-auto rounded" />
+                    <img src={featured.poster} alt={featured.title + " poster"} className="d-block h-100 mx-auto rounded" />
                 </div>
             </div>
             <div className="row justify-content-center">
