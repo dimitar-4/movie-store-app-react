0436e7ac1ee5c4b8055d7e85ae91c8a7ab4a36d6
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-10 17:05:24 +0100

    Add Bag component to navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,32 @@
 import {Link} from "react-router-dom";
+import Bag from "./Bag";
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">The Movie Store</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/movies">Movies</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/orders">Orders</Link>
-                    </li>
-                </ul>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">The Movie Store</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className="nav-link" to="/movies">Movies</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className="nav-link" to="/orders">Orders</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <Bag />
+        </>
     )
 }
 
