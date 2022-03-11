b85a540fed8d4162829105a68df2f882f06af83d
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-11 10:49:28 +0100

    Import BagContext & wrap the App.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import BagContext from './contexts/BagContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <BagContext>
+        <App />
+      </BagContext>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
