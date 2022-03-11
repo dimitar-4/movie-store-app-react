4342ab5e3628a556d6e2c99196e50acb719b455c
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-11 10:57:59 +0100

    Add comps to fix offcanvas, add icons.

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,20 +1,34 @@
+import Portal from "./Portal";
+import {HiOutlineShoppingBag} from "react-icons/hi"
+import { useBag } from "../contexts/BagContext";
+
 function Bag() {
+    const bag = useBag();
+    console.log(bag?.state);
+
     return (
         <>
-            <a className="btn btn-dark ms-auto" data-bs-toggle="offcanvas" href="#bag" role="button" aria-controls="bag">
-                Bag
+            <a className="btn btn-sm btn-dark ms-auto position-relative" data-bs-toggle="offcanvas" href="#bag" role="button" aria-controls="bag">
+                <HiOutlineShoppingBag size={20} />
+                <span className="visually-hidden">shopping bag</span>
+                <span className="position-absolute top-0 start-100 translate-middle badge bg-danger">
+                    0
+                    <span className="visually-hidden">items in shopping bag</span>
+                </span>
             </a>
-            <div className="offcanvas offcanvas-end" tabIndex="-1" id="bag" aria-labelledby="bagLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="bagLabel">Bag</h5>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <div>
-                        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            <Portal>
+                <div className="offcanvas offcanvas-end" tabIndex="-1" id="bag" aria-labelledby="bagLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="bagLabel">Bag</h5>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <div>
+                            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Portal>
         </>
     )
 }
