96267975b65e55b568cbfbaa0009c3369f4df4f0
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 20:28:00 +0100

    Update basket icon & style. Formatting the code.

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,36 +1,54 @@
 import Portal from "./Portal";
-import {HiOutlineShoppingBag} from "react-icons/hi"
+import { GiBasket } from "react-icons/gi";
 import { useBag } from "../contexts/BagContext";
 
 function Bag() {
-    const bag = useBag();
-    console.log(bag?.state);
+  const bag = useBag();
+  console.log(bag?.state);
 
-    return (
-        <>
-            <a className="btn btn-sm btn-dark ms-auto position-relative" data-bs-toggle="offcanvas" href="#bag" role="button" aria-controls="bag">
-                <HiOutlineShoppingBag size={20} />
-                <span className="visually-hidden">shopping bag</span>
-                <span className="position-absolute top-0 start-100 translate-middle badge bg-danger">
-                    0
-                    <span className="visually-hidden">items in shopping bag</span>
-                </span>
-            </a>
-            <Portal>
-                <div className="offcanvas offcanvas-end" tabIndex="-1" id="bag" aria-labelledby="bagLabel">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="bagLabel">Bag</h5>
-                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        <div>
-                            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                        </div>
-                    </div>
-                </div>
-            </Portal>
-        </>
-    )
+  return (
+    <>
+      <a
+        className="btn btn-sm btn-dark ms-auto border-1 border-warning position-relative"
+        data-bs-toggle="offcanvas"
+        href="#bag"
+        role="button"
+        aria-controls="bag"
+      >
+        <GiBasket size={20} />
+        <span className="visually-hidden">shopping bag</span>
+        <span className="position-absolute top-0 start-100 translate-middle badge bg-warning text-dark border-1 border-dark">
+          0<span className="visually-hidden">items in shopping bag</span>
+        </span>
+      </a>
+      <Portal>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex="-1"
+          id="bag"
+          aria-labelledby="bagLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="bagLabel">
+              Shopping Basket
+            </h5>
+            <button
+              type="button"
+              className="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <div>
+              Some text as placeholder. In real life you can have the elements
+              you have chosen. Like, text, images, lists, etc.
+            </div>
+          </div>
+        </div>
+      </Portal>
+    </>
+  );
 }
 
 export default Bag;
