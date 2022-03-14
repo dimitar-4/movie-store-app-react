0168ce0157c4179bc1842110f597d2ad70978ed6
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 20:15:18 +0100

    Update layout on lg devices & main heading.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,50 +2,57 @@ import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 function Movies() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [errors, setErrors] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errors, setErrors] = useState(null);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/api/movies")
-            .then((response) => response.json())
-            .then((json) => {
-                if (json.status === 200) {
-                    setMovies(json.data);
-                } else {
-                    setErrors(json.errors ?? ["Sorry, something went wrong."]);
-                }
-                setLoading(false);
-            })
-            .catch((err) => {
-                setErrors(["Sorry, something went wrong."]);
-                setLoading(false);
-            });
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:8000/api/movies")
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.status === 200) {
+          setMovies(json.data);
+        } else {
+          setErrors(json.errors ?? ["Sorry, something went wrong."]);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setErrors(["Sorry, something went wrong."]);
+        setLoading(false);
+      });
+  }, []);
 
-    if(loading) return (
-        <div className="d-flex justify-content-center">
-            <div className="spinner-border" style={{width: "3rem", height: "3rem"}}>
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    )
-    if (errors && errors.length > 0) return errors.map((e, i) => (
-        <div key={i} className="alert alert-danger">
-            <h4 className="alert-heading">Oops!</h4>
-            <p>{e}</p>
-        </div>
-    ))
+  if (loading)
     return (
-        <div>
-            <h1>Movies</h1>
-            {movies.map((movie) => (
-                <div key={movie.id} className="my-3">
-                    <MovieCard movie={movie} />
-                </div>
-            ))}
+      <div className="d-flex justify-content-center">
+        <div
+          className="spinner-border"
+          style={{ width: "3rem", height: "3rem" }}
+        >
+          <span className="visually-hidden">Loading...</span>
         </div>
-    )
+      </div>
+    );
+  if (errors && errors.length > 0)
+    return errors.map((e, i) => (
+      <div key={i} className="alert alert-danger">
+        <h4 className="alert-heading">Oops!</h4>
+        <p>{e}</p>
+      </div>
+    ));
+  return (
+    <div>
+      <h1 className="text-uppercase text-center">Welcome to the Movies</h1>
+      <div className="row">
+        {movies.map((movie) => (
+          <div key={movie.id} className="col col-lg-6 col-xll-4 my-3">
+            <MovieCard movie={movie} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Movies;
