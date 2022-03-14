387819b80feb7ccf2a077136a80fd96d10a2687d
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 20:30:09 +0100

    Add brand icon. Update navbar style.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,55 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Bag from "./Bag";
+import { GiPopcorn } from "react-icons/gi";
 
 function Navbar() {
-    return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">The Movie Store</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                            <Link className="nav-link" to="/movies">Movies</Link>
-                            </li>
-                            <li className="nav-item">
-                            <Link className="nav-link" to="/orders">Orders</Link>
-                            </li>
-                        </ul>
-                        <Bag />
-                    </div>
-                </div>
-            </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light text-light bg-dark mb-4">
+      <div className="container-fluid">
+        <Link
+          className="navbar-brand text-light fs-3 ms-4 d-flex align-items-center"
+          to="/"
+        >
+          Screen Goldies <GiPopcorn className="text-warning fs-3" />
+        </Link>
+        <button
+          className="navbar-toggler bg-warning"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav text-uppercase fw-normal ms-5">
+            <li className="nav-item">
+              <Link
+                className="nav-link text-light active"
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-light" to="/movies">
+                Movies
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-light" to="/orders">
+                Orders
+              </Link>
+            </li>
+          </ul>
+          <Bag />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
