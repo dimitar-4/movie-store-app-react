3557fb1822c67a20b561a63e1a499a7b0ac4f285
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 20:18:58 +0100

    Add icons, update btns style & content.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,31 +1,46 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { BsInfoCircle } from "react-icons/bs";
+import { GiTwoCoins } from "react-icons/gi";
 
 function MovieCard({ movie }) {
-    return (
-        <div className="bg-dark text-white rounded d-flex">
-            <div>
-                <img src={movie.poster} alt={movie.title + " poster"} className="d-flex h-100 rounded" style={{width: 175}} />
-            </div>
-            <div className="p-4 flex-grow-1">
-                <h2>{movie.title}</h2>
-                <div className="d-flex">
-                    <p className="me-1">{movie.year}</p>
-                    <p className="me-1">|</p>
-                    <p>{movie.genres.map((g, i) => i === movie.genres.length - 1 ? g : g + ", ")}</p>
-                </div>
-                <p>
-                    {movie.description}
-                </p>
-                <div className="d-flex align-items-center justify-content-between">
-                    <p className="fw-bold m-0">{movie.price} {movie.currency}</p>
-                    <div>
-                        <Link to={"/movies/" + movie.id} className="btn btn-light me-2">Details</Link>
-                        <button className="btn btn-primary">Purchase</button>
-                    </div>
-                </div>
-            </div>        
+  return (
+    <div className="bg-dark text-white rounded d-flex h-100">
+      <div>
+        <img
+          src={movie.poster}
+          alt={movie.title + " poster"}
+          className="d-flex h-100 rounded"
+          style={{ width: 175 }}
+        />
+      </div>
+      <div className="p-4 flex-grow-1">
+        <h2>{movie.title}</h2>
+        <div className="d-flex">
+          <p className="me-1">{movie.year}</p>
+          <p className="me-1">|</p>
+          <p>
+            {movie.genres.map((g, i) =>
+              i === movie.genres.length - 1 ? g : g + ", "
+            )}
+          </p>
         </div>
-    )
+        <p>{movie.description}</p>
+        <div className="d-flex align-items-center justify-content-between">
+          <p className="fw-bold m-0">
+            {movie.price} {movie.currency}
+          </p>
+          <div>
+            <Link to={"/movies/" + movie.id} className="btn btn-light me-2">
+              <BsInfoCircle className="mb-1" /> Info
+            </Link>
+            <button className="btn btn-warning">
+              <GiTwoCoins /> Buy
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
