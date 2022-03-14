87028c3671f17352e774f937bee6496c3f6c1586
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 20:42:33 +0100

    Add contact & follow p. Update style & format code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,32 @@
-function Footer() {
-    const date = new Date();
+import { GiPopcorn } from "react-icons/gi";
 
-    return (
-        <footer className="py-4 bg-dark text-white mt-auto">
-            <p className="text-center m-0">Copyright &copy; - {date.getFullYear()} The Movie Store</p>
-            <p style={{fontSize: 12}} className="text-center m-0 fw-bold fst-italic">All Rights Reserved</p>
-        </footer>
-    )
+function Footer() {
+  const date = new Date();
+
+  return (
+    <footer className="py-4 bg-dark text-white mt-auto">
+      <div className="d-flex justify-content-evenly align-items-center">
+        <div>
+          <p>Contact Us</p>
+        </div>
+        <div>
+          <p className="text-center m-0 d-flex align-items-center">
+            Copyright &copy; - {date.getFullYear()} Screen Goldies{" "}
+            <GiPopcorn className="text-warning" />
+          </p>
+          <p
+            style={{ fontSize: 12 }}
+            className="text-center text-warning m-0 fw-bold fst-italic"
+          >
+            All Rights Reserved
+          </p>
+        </div>
+        <div>
+          <p>Follow Us</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
