595b87a537827f96475ca9b16b11b50c6860f668
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-14 11:46:09 +0100

    Create component, add spinner while loading.

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+function Spinner({ size = 3 }) {
+  return (
+    <div className="d-flex justify-content-center">
+      <div
+        className="spinner-border"
+        style={{ width: size + "rem", height: size + "rem" }}
+      >
+        <span className="visually-hidden">loading...</span>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
