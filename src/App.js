576003cad08feaea9fb3c0ee0a1accb4891d5ea9
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-15 16:19:29 +0100

    Add Checkout route & update app bg-color.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import MovieDetails from "./components/MovieDetails";
@@ -9,7 +10,7 @@ import Orders from "./components/Orders";
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className="d-flex flex-column min-vh-100 bg-light">
       <Navbar />
       <div className="container">
         <Routes>
@@ -18,6 +19,7 @@ function App() {
           <Route path="/movies/:movieId" element={<MovieDetails />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/orders/:orderId" element={<OrderDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </div>
       <Footer />
