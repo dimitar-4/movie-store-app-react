8537f8eba3d5c4393ba218465f2659a44430e91e
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-15 10:19:42 +0100

    Add icon, BS style classes & text content.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { GiPopcorn } from "react-icons/gi";
+import { GrMail } from "react-icons/gr";
 
 function Footer() {
   const date = new Date();
@@ -7,12 +8,19 @@ function Footer() {
     <footer className="py-4 bg-dark text-white mt-auto">
       <div className="d-flex justify-content-evenly align-items-center">
         <div>
-          <p>Contact Us</p>
+          <p className="m-0 text-center">Contact Us</p>
+          <p className="text-warning m-0">
+            <GrMail />{" "}
+            <strong style={{ fontSize: 12 }} className="text-center fst-italic">
+              customer.service@sgms.net
+            </strong>
+          </p>
         </div>
         <div>
           <p className="text-center m-0 d-flex align-items-center">
-            Copyright &copy; - {date.getFullYear()} Screen Goldies{" "}
+            {date.getFullYear()} Screen Goldies
             <GiPopcorn className="text-warning" />
+            &copy;
           </p>
           <p
             style={{ fontSize: 12 }}
@@ -22,7 +30,8 @@ function Footer() {
           </p>
         </div>
         <div>
-          <p>Follow Us</p>
+          <p className="m-0 text-center">Follow Us</p>
+          <p className="text-warning m-0"></p>
         </div>
       </div>
     </footer>
