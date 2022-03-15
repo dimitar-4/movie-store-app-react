94bb05d0639509f50e0c116a1d40a0b7ef518157
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-15 10:17:09 +0100

    Add ADD or UPDATE functionality.

--- src/contexts/bagReducer.js
+++ src/contexts/bagReducer.js
@@ -1,3 +1,34 @@
+import { types } from "./BagContext";
+
 export function bagReducer(state, action) {
-    return state;
+    switch (action.type) {
+        case types.ADD:
+            return {
+                ...state,
+                movies: addOrUpdate(state.movies, action.payload)
+            };
+        case types.REMOVE:
+            return state;
+        case types.CLEAR:
+            return {
+                ...state,
+                movies: [],
+                totalAmount: 0.00
+            };
+        default:
+            return state;
+    }
+}
+
+function addOrUpdate(movies, movie) {
+    const index = movies.findIndex((m) => m.id === movie.id);
+    if (index === -1) {
+        return [...movies, { ...movie, quantity: 1 }]
+    } else {
+        return [
+            ...movies.slice(0, index),
+            { ...movies[index], quantity: movies[index].quantity + 1 },
+            ...movies.slice(index + 1), 
+        ];
+    }
 }
