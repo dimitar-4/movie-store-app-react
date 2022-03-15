73682995c3ad0cf51f842f70850bddbc05fc08c0
Dimitar Cholakov <dcholakov4@gmail.com>
2022-03-15 10:13:04 +0100

    Creat add/remove/clear types & functionality.

--- src/contexts/BagContext.jsx
+++ src/contexts/BagContext.jsx
@@ -1,31 +1,49 @@
 import { bagReducer } from "./bagReducer";
-
 import { createContext, useReducer, useContext } from "react";
 
+export const types = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+  CLEAR: "CLEAR",
+};
+
 const Bag = createContext({
-    state: {
-        movies: [],
-        totalAmount: "0.00",
-    },
-    dispatch: () => {}
+  state: {
+    movies: [],
+    totalAmount: "0.00",
+  },
+  dispatch: () => {},
+  addToBag: (movie) => {},
+  clearBag: () => {},
 });
 
 function BagContext({ children }) {
-    const [state, dispatch] = useReducer(bagReducer, {
-        movies: [],
-        totalAmount: "0.00",
-    });
+  const [state, dispatch] = useReducer(bagReducer, {
+    movies: [],
+    totalAmount: "0.00",
+  });
 
-    return <Bag.Provider value={{ state, dispatch }}>{children}</Bag.Provider>;
+  function addToBag(movie) {
+    dispatch({ type: types.ADD, payload: movie });
+  }
+
+  function clearBag() {
+    dispatch({ type: types.CLEAR });
+  }
+
+  return (
+    <Bag.Provider value={{ state, dispatch, addToBag, clearBag }}>
+      {children}
+    </Bag.Provider>
+  );
 }
 
 export function useBag() {
-    const context = useContext(Bag);
-    if (context === undefined) {
-        throw new Error(" useBag must be used within a BagProvider")
-    }
-    return context
+  const context = useContext(Bag);
+  if (context === undefined) {
+    throw new Error(" useBag must be used within a BagProvider");
+  }
+  return context;
 }
 
 export default BagContext;
-
